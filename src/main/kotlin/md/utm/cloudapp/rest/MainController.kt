8fa2f299c9157f2f83b1fb5275eb460e8f566f01
MrCrowley21 <graur.lenka@gmail.com>
2025-06-04 00:47:08 +0300

    fix: adding rediness check

--- src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
+++ src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
@@ -12,7 +12,7 @@ class MainController {
     @GetMapping("/")
     fun main(): String {
         logger.info("Received request on /")
-        val response = "Hello World in the deploy redone again!!!"
+        val response = "Hello World!!! I worked so hard on this lab!"
         logger.debug("Response: $response")
         return response
     }
